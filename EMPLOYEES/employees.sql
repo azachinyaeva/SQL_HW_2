0aaacb46ae61231e4d9f58e928cb3e20d3f67640
Anna <annahasagun@yahoo.com>
2023-06-23 07:40:05 +0300

    fix reference

--- EMPLOYEES/employees.sql
+++ EMPLOYEES/employees.sql
@@ -2,8 +2,6 @@
 CREATE TABLE IF NOT EXISTS Employee (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(60) NOT NULL,
-	manager_id INTEGER,
+	manager_id INTEGER REFERENCES Employee (id),
 	department_id INTEGER 
 );
-
-
